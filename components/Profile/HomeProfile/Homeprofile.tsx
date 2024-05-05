a63e8cfee775e695223d8cb2cb1dd854d6595d61
vermaanurag1532 <104077846+vermaanurag1532@users.noreply.github.com>
2024-05-05 16:06:08 +0530

    add: Home Page

--- components/Profile/HomeProfile/Homeprofile.tsx
+++ components/Profile/HomeProfile/Homeprofile.tsx
@@ -24,8 +24,10 @@ const HomeProfile = () => {
       <Card.Section
         h={140}
         style={{
-          backgroundImage:
-            'url(https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80)',
+          backgroundImage: 'url(assets/Images/logo1.jpg)',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          height: '200px'
         }}
       />
       <Avatar
